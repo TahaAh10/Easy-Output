8dc80641870654579c5f8cea780d8bcd8080feed
TahaAh10 <162183332+TahaAh10@users.noreply.github.com>
2024-03-16 14:23:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// Define a function to greet a person
+function greet(name) {
+    console.log("Hello, ".concat(name, "!"));
+}
+// Call the greet function
+var personName = "Web Developers";
+greet(personName);
